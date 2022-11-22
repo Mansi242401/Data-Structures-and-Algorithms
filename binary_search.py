6833d499884bdd132cf63c855b0d53f43163e62e
Mansi242401 <118791563+Mansi242401@users.noreply.github.com>
2022-11-21 21:47:30 -0800

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,1 +1,15 @@
 
+class solution:
+  def binary_search(self,input_array:list[int],value:int) -> int:
+    n = len(input_array)
+    l,r = 0,n-1
+    while l <= r:
+      mid = (l+r) // 2
+      if value == input_array[mid]:
+        return mid
+      elif value > input_array[mid]:
+        l = mid+1
+      else:
+        r = mid-1
+     return -1
+        
